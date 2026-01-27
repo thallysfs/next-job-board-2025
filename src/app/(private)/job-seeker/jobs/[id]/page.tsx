ea4f2b67695e5045768259564c0a15b0ebc2e842
thallysfs <thallys@hotmail.com>
2026-01-27 11:08:31 -0300

    feat: add elemntes in details job page

--- src/app/(private)/job-seeker/jobs/[id]/page.tsx
+++ src/app/(private)/job-seeker/jobs/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getJobById } from '@/actions/jobs'
 import InfoMessage from '@/components/ui/info-message'
 import { IJob } from '@/interfaces'
+import dayjs from 'dayjs'
+import { Banknote, Calendar, MapPin } from 'lucide-react'
 import React from 'react'
 
 interface JobDetailsPageProps {
@@ -20,17 +22,56 @@ async function JobDetailsPage({ params }: JobDetailsPageProps) {
 
   return (
     <div className='flex flex-col gap-5'>
-      <div className="flex justify-between">
-        <div>
-          <h1 className="text-sm font-bold text-gray-700">
-            {job.title}
-          </h1>
-          <h1 className="text-xs text-gray-500">
-            De: {job.recruiter.name}
-          </h1>
+      <div>
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-sm font-bold text-gray-700">
+              {job.title}
+            </h1>
+            <h1 className="text-xs text-gray-500">
+              De: {job.recruiter.name}
+            </h1>
+            <div className='h-max bg-blue-100 text-blue-600 border border-blue-600 px-2 py-1 rounded capitalize text-xs font-medium items-center'>
+              {job.job_type.replace("-", " ")}
+            </div>
+            <div className='flex flex-wrap'>
+              {job.skills.map((skill, index) => (
+                <div key={index} className='bg-gray-200 border border-gray-400 text-gray-700 px-2 py-1 rounded text-xs font-medium mr-2'>
+                  {skill}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className='h-max bg-blue-100 text-blue-600 border border-blue-600 px-2 py-1 rounded capitalize text-xs font-medium items-center'>
-          {job.job_type.replace("-", " ")}
+      </div>
+
+      <hr
+        className='border border-gray-300'
+      />
+
+      <div className="flex justify-between">
+        <div className='flex items-center'>
+          <MapPin size={16} className='text-gray-600' />
+          <span className='text-sm text-gray-600 ml-1'>{job.location}</span>
+        </div>
+
+        <div className='flex items-center'>
+          <Banknote size={16} className='text-gray-600' />
+          <span className='text-sm text-gray-600 ml-1'>${job.min_salary} - ${job.max_salary}</span>
+        </div>
+
+        <div className='flex items-center'>
+          <Calendar size={16} className='text-gray-600' />
+          <span className='text-sm text-gray-600 ml-1'>Data máxima para aplicar - {dayjs(job.last_date_to_apply).format('DD MMM YYYY')}</span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 border border-gray-300 p-5 rounded-xl">
+          <p
+            className='text-gray-700 whitespace-pre-line text-sm'
+            dangerouslySetInnerHTML={{ __html: job.description }}
+          ></p>
         </div>
       </div>
     </div>
